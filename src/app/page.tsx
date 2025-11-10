50ebd561118159091448b18b708927b1badd2030
kishansinghifs1 <kishansingh956196@gmai.com>
2025-11-10 22:33:15 +0530

    feat(auth): integrate better-auth for authentication and user management
    - Updated Prisma schema to include User, Session, Account, and Verification models.
    - Added authentication utilities for requiring authentication and unauthenticated access.
    - Implemented login and signup pages with forms for user input.
    - Created API routes for authentication using better-auth.
    - Enhanced TRPC procedures to protect user data access.
    - Added a Toaster component for user notifications.
    - Updated package.json with new dependencies for better-auth and related libraries.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,15 @@
-import { Button } from '@/components/ui/button'
-import prisma from '@/lib/db'
-import { caller, getQueryClient, trpc } from '@/trpc/server'
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
-import React, { Suspense } from 'react'
-import { Client } from './client'
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 
-const page = async() => {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
-  return (
+const Page = async() => {
+  await requireAuth();
+  const data = await  caller.getUsers() 
+ return (
    <div className="min-h-screen min-w-screen flex items-center justify-center">
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>loading...</p>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+       Protected Server Component
+       {JSON.stringify(data)}
   </div>
   )
 }
   
-export default page
+export default Page
